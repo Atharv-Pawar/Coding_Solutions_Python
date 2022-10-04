fc3df80e909b43d3e6ad3300269f38147c2a47fd
Atharv <atharv111p@gmail.com>
2022-10-04 09:13:51 +0530

    Number Mirror

---
+++ codechef002.py
@@ -0,0 +1,11 @@
+"""
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+"""
+
+print(int(input()))

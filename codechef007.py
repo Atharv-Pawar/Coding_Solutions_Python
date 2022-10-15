bd8c3782a7fd6dd59a3c2c10d55cab6aead58397
Atharv <atharv111p@gmail.com>
2022-10-15 14:19:51 +0530

    Cutting Paper

---
+++ codechef007.py
@@ -0,0 +1,31 @@
+"""
+Chef has a square-shaped chart paper with the side length equal to N. He wants to cut out K times K×K squares from this chart paper.
+
+Find the maximum number of K times K×K squares he can cut from the entire chart paper.
+
+Note that, some part of the chart paper might not be a included in any K times K×K cutout square.
+
+Input Format
+The first line contains a single integer T — the number of test cases. Then the test cases follow.
+The first and only line of each test case contains two space-separated integers N and K — the side length of the entire chart paper and the side length of the cutout squares.
+Output Format
+For each test case, output on a new line the maximum number of K times K×K squares Chef can cut from the entire chart paper.
+"""
+"""
+Input                   Output
+3                       25
+5 1                      1
+2 2                      4
+5 2
+"""
+
+T = int(input())
+if 1<=T<=1000:
+    for _ in range(T):
+        N, K = map(int, input().split())
+        if 1<=K<=1000 and 1<=N<=1000:
+            print((N//K)*(N//K)) # different logic
+        else:
+            print('Invalid Input')
+else:
+    print('Invalid Input')

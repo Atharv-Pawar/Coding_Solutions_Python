e9936fba8db764e23861cfdce490d18c6bc59f03
Atharv <atharv111p@gmail.com>
2022-10-29 23:56:55 +0530

    Reachable Integer

---
+++ codechef010.py
@@ -0,0 +1,12 @@
+"""
+You are given a range of integers {L,L+1,…,R}. An integer X is said to be reachable if it can be represented as a sum of two not necessarily distinct integers in this range. Find the number of distinct reachable integers.
+
+Input
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first and only line of each test case contains two space-separated integers L and R.
+Output
+For each test case, print a single line containing one integer — the number of reachable integers.
+"""
+for _ in range(int(input())):
+    l, r = map(int, input().split(' '))
+    print((2*r)-(2*l)+1) #final logic
